52fa0d0f9d9ed4c0eb4ded9538b8b415a1254ced
Malith Ranasinghe <malithmp@malithmp.local>
2014-05-11 01:05:12 -0400

    Mac to ubuntu

--- python_ftp/MyHandler.py
+++ python_ftp/MyHandler.py
@@ -3,14 +3,20 @@ import os
 from glbl import *
 import cgi
 
+def parsePost(postData):
+	# PURPOSE: Parse data String and return needed data
+	# INPUT FORMAT: data1=value1&data3=value2...etc
+	# OUTPUT: Dictionary of key value pairs
+	outputDict = {}
+	data =postData.split('&')
+	for item in data:
+		(key,value)=item.split('=')	
+		outputDict[key]=value
+	return outputDict
+	
 class MyHandler(BaseHTTPRequestHandler):
-
+		
 	def do_GET(self):
-
-		printX()
-		incrementX()
-		printX()
-
 		print ('Path', os.getcwd())
 		try:
 			print('Do Get',self.path)
@@ -32,9 +38,17 @@ class MyHandler(BaseHTTPRequestHandler):
 		postvars={}
 		try:
 			print('Do Post')
-			content_len = int(self.headers.getheader('content-length'))
+			content_len = int(self.headers['content-length'])
 			post_body = self.rfile.read(content_len)
 			print ('POST VAR:' ,post_body)
+			data =  parsePost(post_body)
+			print (data)
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()	
+			self.wfile.write("RESPONSE\n".encode('utf-8'))
 
 		except IOError as e :
 			print('Do Post Error')
+
+                        
